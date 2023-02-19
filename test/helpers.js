f358a4598c6fd9b331fc7a1455f9d30bd7687470
RiayaEdy <edywidowaty@gmail.com>
2023-02-19 21:46:01 +1100

    removing altering and inserting html

--- test/helpers.js
+++ test/helpers.js
@@ -1,9 +1,11 @@
 const chai = require('chai')
+
 global.expect = chai.expect
 const fs = require('file-system')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
 const babel = require('babel-core');
+
 
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
 
